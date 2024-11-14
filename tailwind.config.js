0e629a334acd410b85b0057d99523fe5c72ff61f
ZaripovDostonbek <dastandev0110@gmail.com>
2024-11-14 21:11:53 +0500

    Loyiha tugallandi

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const { default: ColorName } = require("color-name");
+
+module.exports = {
+  content: ["./pages/**/*.{html,js}"],
+  theme: {
+    fontFamily:{
+      inter:"Inter",
+    },
+    extend: {
+      colors:{
+        1:"#FF9846",
+        2:"#A4ACAD",
+        3:"#000",
+        4:"#EB5757",
+      },
+    },
+  },
+  plugins: [],
+}
+
